a5e7e5fbf91263d3bdb38968e2de799316324e2a
sameer <mohammedsamee89@gmail.com>
2026-03-13 11:33:27 +0530

    Added HOME_PAGE,LOGIN_PAGE,REGISTER_PAGE

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "base.html")
+def login_view(request):
+    return render(request, 'auth/login.html')
+def register_view(request):
+    return render(request, 'auth/register.html')
 
