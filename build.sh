f70d2a984991f57a7d10f00a29c9acb8ebc2f921
Yusuf Ali <yusufali2235@gmail.com>
2026-04-25 01:14:33 +0530

    Clean up build.sh by removing unused commands
    Removed unnecessary commands for backend directory navigation and static file collection.

--- build.sh
+++ build.sh
@@ -2,9 +2,3 @@
 set -o errexit
 
 pip install -r backend/requirements.txt
-
-cd backend
-
-# Only collect static files - skip database operations
-# Database migrations will be run locally before deployment
-python manage.py collectstatic --noinput --no-input
